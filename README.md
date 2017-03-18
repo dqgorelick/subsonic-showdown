3c1cec4ff1f7333bb2e304766c5bfbd80e73593b
Daniel Gorelick <dqgorelick@gmail.com>
2017-03-17 23:46:38 -0400

    adds webpack for es6

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 Final Name TBD, alternative name: Sub-dominant
 
+
+## Setup
+
+#### Install dependencies:
+
+run `npm install`
+
+##### Run webpack (transpiles es6 and sass)
+
+run `npm start` or `npm run start`
+
+##### Run dev server
+
+run `python -m SimpleHTTPServer 9999` or whichever static server suits your fancy
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    './src/app'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: 'dist/'
+  },
+  plugins: [
+    new ExtractTextPlugin('main.css', { 
+      allChunks: true,
+    })
+  ],
+  module: {
+    loaders: [{
+        test: /\.js$/,
+        loaders: [
+          'babel-loader'
+        ],
+        include: path.join(__dirname, 'src')
+      }, {
+        test: /\.html$/,
+        loader: 'raw',
+        exclude: /node_modules/
+      },
+      {    test: /\.scss?$/,    loader: ExtractTextPlugin.extract('css-loader!sass-loader'),    }, {
+        test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+        loader: 'file?name=public/fonts/[name].[ext]'
+      }, {
+        test: /\.(png|jpg|gif|jpeg)$/,
+        loader: 'file-loader'
+      }
+    ],
+    postLoaders: [{
+      include: path.resolve(__dirname, 'node_modules/pixi.js'),
+      loader: 'transform?brfs'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json', '.scss', 'sass', 'css']
+  }
+};
